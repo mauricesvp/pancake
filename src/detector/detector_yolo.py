80c6e9431ae833683d607fefa19e893a26cd9ffa
mauricesvp <maurice@walny.de>
2021-05-14 17:17:22 +0200

    Add very simple detector

--- src/detector/detector_yolo.py
+++ src/detector/detector_yolo.py
@@ -1,10 +1,17 @@
 """Detector based on YOLOv5."""
-from pancake.detector.detector import Detector
+import torch
+
+from .detector import Detector
 
 
 class YOLODetector(Detector):
-    def __init__(self, *args, **kwargs) -> None:
-        pass
+    """Very simple detector using pretrained yolov5."""
 
-    def detector(self, img) -> list:
-        return []
+    def __init__(self, *args, **kwargs) -> None:
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5l', pretrained=True)
+
+    def detect(self, imgs) -> list:
+        if type(imgs) is not list:
+            imgs = [imgs]
+        res = self.model(imgs)
+        return res.xyxy
