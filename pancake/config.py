267095b1bb1406973808e7b9da7688170b81d00d
mauricesvp <maurice@walny.de>
2021-05-19 02:30:16 +0200

    Add logger; init config

--- pancake/config.py
+++ pancake/config.py
@@ -1,1 +1,10 @@
-"""Configuration goes here."""
+import configparser
+import os
+
+
+def get_config() -> configparser.ConfigParser:
+    config = configparser.ConfigParser()
+    basedir = os.path.dirname(__file__)
+    config_path = os.path.join(basedir, "pancake.cfg")
+    config.read(config_path)
+    return config
