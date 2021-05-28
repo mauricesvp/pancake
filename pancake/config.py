375dba9a83cae9e187f1b84a1f9d900b7e7902d4
tuananhroman <anh.maxiking@hotmail.de>
2021-05-28 23:19:29 +0200

    option to parse arbitrary pancake cfg path (convenient for colab)

--- pancake/config.py
+++ pancake/config.py
@@ -3,7 +3,7 @@ import configparser
 import os
 
 
-def pancake_config() -> configparser.ConfigParser:
+def pancake_config(cfg_path: str=None) -> configparser.ConfigParser:
     """Parse config file."""
     # Using pancake.cfg
     # config = configparser.ConfigParser()
@@ -13,6 +13,7 @@ def pancake_config() -> configparser.ConfigParser:
     # return config
 
     # Using pancake.yaml
-    basedir = os.path.dirname(__file__)
-    config_path = os.path.join(basedir, "pancake.yaml")
-    return get_config(config_path)
+    if cfg_path is None:
+        basedir = os.path.dirname(__file__)
+        cfg_path = os.path.join(basedir, "pancake.yaml")
+    return get_config(cfg_path)
