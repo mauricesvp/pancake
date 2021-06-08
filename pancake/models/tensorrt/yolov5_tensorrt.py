57bf77743845801bd4f9af47b04bc7264246e513
tuananhroman <anh.maxiking@hotmail.de>
2021-06-08 11:19:05 +0200

    create, save, load tensorrt engine from provided model

--- pancake/models/tensorrt/yolov5_tensorrt.py
+++ pancake/models/tensorrt/yolov5_tensorrt.py
@@ -1,29 +1,36 @@
 import numpy as np
 import os
 import pkg_resources
+
+import pycuda.driver as cuda
+import pycuda.autoinit
 import torch
 import torch.nn as nn
 from typing import Type
 
+
 from pancake.logger import setup_logger
 from pancake.models.base_class import BaseModel
-from .trt_utils import export_onnx
+from pancake.utils.general import export_onnx
 
 l = setup_logger(__name__)
 
-for package in ['tensorrt']:
+for package in ["tensorrt"]:
     try:
         dist = pkg_resources.get_distribution(package)
-        l.info(u'\u2713 ' + '{} ({}) is installed'.format(dist.key, dist.version)) 
-        import tensorrt
+        l.info("\u2713 " + "{} ({}) is installed".format(dist.key, dist.version))
+        import tensorrt as trt
+
         trt_installed = True
+
+        TRT_LOGGER = trt.Logger(trt.Logger.ERROR)
+        EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
     except pkg_resources.DistributionNotFound:
-        l.info(u'\u2620 ' + '{} is NOT installed'.format(package))
+        l.info("\u2620 " + "{} is NOT installed".format(package))
         trt_installed = False
 
 
 class Yolov5TRT(BaseModel):
-
     def __init__(self, yolov5, weights_path):
         # if trt not available return standard yolov5 model
         if not trt_installed:
@@ -32,18 +39,11 @@ class Yolov5TRT(BaseModel):
 
         self.yolov5 = yolov5
 
-        # initialize model for export
-        tmp_model = Yolov5TRT._init_export(
-            self.yolov5.model.float()
-        )
-
         # TRT currently only supports non-batch inference
         batch_size = 1
         x = self.yolov5._required_img_size
 
-        input_tensor = (
-            torch.zeros(batch_size, 3, x, x).float().to(self.yolov5._device)
-        )
+        input_tensor = torch.zeros(batch_size, 3, x, x).float().to(self.yolov5._device)
 
         onnx_path = (
             weights_path.replace(".pt", ".onnx")
@@ -52,18 +52,30 @@ class Yolov5TRT(BaseModel):
         )
 
         weights_name = onnx_path.split("/")[-1].split(".")[0]
-        
+
         l.info(f"Converting PyTorch model from weights {weights_name} to ONNX")
-        if not export_onnx(tmp_model, onnx_path, input_tensor):
+        if not export_onnx(
+            self._init_export(), onnx_path, input_tensor, dynamic_axes=False
+        ):
             l.info("Couldn't convert to ONNX, using standard Yolov5..")
             return self.yolov5
-        
-    
-    @staticmethod
-    def _init_export(model):
+
+        # TRT init
+
+        # NMS on GPU
+        # max supported is 4096, if conf_thres is low, such as 0.001, use larger number.
+        self._topK = 512
+        self._keepTopK = 300  # max detections on nms
+
+        self.build_engine(onnx_path)
+
+    def _init_export(self):
         from pancake.utils.activations import Hardswish, SiLU
         from pancake.models.yolo import Detect
         from pancake.models.common import Conv
+
+        assert self.yolov5
+        model = self.yolov5.model.float()
 
         for k, m in model.named_modules():
             m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatibility
@@ -75,6 +87,7 @@ class Yolov5TRT(BaseModel):
             elif isinstance(m, Detect):
                 m.inplace = opt.inplace
                 m.onnx_dynamic = opt.dynamic
+
         return model
 
     def _init_infer(self, img_size):
@@ -85,7 +98,6 @@ class Yolov5TRT(BaseModel):
         """
         pass
 
-
     def prep_image_infer(self, img: Type[np.array]) -> Type[torch.Tensor]:
         """
         Preprocesses images for inference (on device, expanded dim (,4), half precision (fp16), normalized)
@@ -94,7 +106,6 @@ class Yolov5TRT(BaseModel):
         :return prep_img: preprocessed image
         """
         return super(Yolov5Model, self).prep_image_infer(img)
-
 
     def infer(self, img: Type[np.array]) -> Type[torch.Tensor]:
         """
@@ -105,3 +116,198 @@ class Yolov5TRT(BaseModel):
                               expanded dim (,4), half precision (fp16))
         """
         pass
+
+    def build_engine(self, onnx_path, using_half: bool = True):
+        trt.init_libnvinfer_plugins(None, "")
+        engine_file = onnx_path.replace(".onnx", ".trt")
+        num_classes = len(self.yolov5.names)
+
+        # read existing trt engine
+        if os.path.exists(engine_file):
+            engine_exists = True
+            l.info(f"Found a corresponding TRT engine at {engine_file}")
+            with open(engine_file, "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
+                self.engine = runtime.deserialize_cuda_engine(f.read())
+                return
+                
+        with trt.Builder(TRT_LOGGER) as builder, builder.create_network(
+            EXPLICIT_BATCH
+        ) as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
+            builder.max_batch_size = 1  # always 1 for explicit batch
+            config = builder.create_builder_config()
+            config.max_workspace_size = 1 * 1 << 30
+            if using_half:
+                config.set_flag(trt.BuilderFlag.FP16)
+            # Load the Onnx model and parse it in order to populate the TensorRT network.
+            with open(onnx_path, "rb") as model:
+                if not parser.parse(model.read()):
+                    l.info(
+                        "ERROR: Failed to parse the ONNX file, using standard Yolov5.."
+                    )
+                    for error in range(parser.num_errors):
+                        l.info(parser.get_error(error))
+                    return self.yolov5
+
+            previous_output = network.get_output(0)
+            network.unmark_output(previous_output)
+
+            # slice boxes, obj_score, class_scores
+            strides = trt.Dims([1, 1, 1])
+            starts = trt.Dims([0, 0, 0])
+            bs, num_boxes, _ = previous_output.shape
+            shapes = trt.Dims([bs, num_boxes, 4])
+            boxes = network.add_slice(previous_output, starts, shapes, strides)
+            starts[2] = 4
+            shapes[2] = 1
+            obj_score = network.add_slice(previous_output, starts, shapes, strides)
+            starts[2] = 5
+            shapes[2] = num_classes
+            scores = network.add_slice(previous_output, starts, shapes, strides)
+
+            indices = network.add_constant(
+                trt.Dims([num_classes]), trt.Weights(np.zeros(num_classes, np.int32))
+            )
+            gather_layer = network.add_gather(
+                obj_score.get_output(0), indices.get_output(0), 2
+            )
+
+            # scores = obj_score * class_scores => [bs, num_boxes, nc]
+            updated_scores = network.add_elementwise(
+                gather_layer.get_output(0),
+                scores.get_output(0),
+                trt.ElementWiseOperation.PROD,
+            )
+
+            # reshape box to [bs, num_boxes, 1, 4]
+            reshaped_boxes = network.add_shuffle(boxes.get_output(0))
+            reshaped_boxes.reshape_dims = trt.Dims([0, 0, 1, 4])
+
+            # add batchedNMSPlugin, inputs:[boxes:(bs, num, 1, 4), scores:(bs, num, 1)]
+            trt.init_libnvinfer_plugins(TRT_LOGGER, "")
+            registry = trt.get_plugin_registry()
+            assert registry
+            creator = registry.get_plugin_creator("BatchedNMS_TRT", "1")
+            assert creator
+            fc = []
+            fc.append(
+                trt.PluginField(
+                    "shareLocation",
+                    np.array([1], dtype=np.int),
+                    trt.PluginFieldType.INT32,
+                )
+            )
+            fc.append(
+                trt.PluginField(
+                    "backgroundLabelId",
+                    np.array([-1], dtype=np.int),
+                    trt.PluginFieldType.INT32,
+                )
+            )
+            fc.append(
+                trt.PluginField(
+                    "numClasses",
+                    np.array([num_classes], dtype=np.int),
+                    trt.PluginFieldType.INT32,
+                )
+            )
+            fc.append(
+                trt.PluginField(
+                    "topK",
+                    np.array([self._topK], dtype=np.int),
+                    trt.PluginFieldType.INT32,
+                )
+            )
+            fc.append(
+                trt.PluginField(
+                    "keepTopK",
+                    np.array([self._keepTopK], dtype=np.int),
+                    trt.PluginFieldType.INT32,
+                )
+            )
+            fc.append(
+                trt.PluginField(
+                    "scoreThreshold",
+                    np.array([self.yolov5._conf_thres], dtype=np.float32),
+                    trt.PluginFieldType.FLOAT32,
+                )
+            )
+            fc.append(
+                trt.PluginField(
+                    "iouThreshold",
+                    np.array([self.yolov5._iou_thres], dtype=np.float32),
+                    trt.PluginFieldType.FLOAT32,
+                )
+            )
+            fc.append(
+                trt.PluginField(
+                    "isNormalized",
+                    np.array([0], dtype=np.int),
+                    trt.PluginFieldType.INT32,
+                )
+            )
+            fc.append(
+                trt.PluginField(
+                    "clipBoxes", np.array([0], dtype=np.int), trt.PluginFieldType.INT32
+                )
+            )
+
+            fc = trt.PluginFieldCollection(fc)
+            nms_layer = creator.create_plugin("nms_layer", fc)
+
+            layer = network.add_plugin_v2(
+                [reshaped_boxes.get_output(0), updated_scores.get_output(0)], nms_layer
+            )
+            layer.get_output(0).name = "num_detections"
+            layer.get_output(1).name = "nmsed_boxes"
+            layer.get_output(2).name = "nmsed_scores"
+            layer.get_output(3).name = "nmsed_classes"
+            for i in range(4):
+                network.mark_output(layer.get_output(i))
+
+            # build new trt engine
+            self.engine = builder.build_engine(network, config)
+
+            # serialize and store the engine
+            with open(engine_file, "wb") as f:
+                f.write(self.engine.serialize())
+
+                from pancake.utils.general import file_size
+                l.info(
+                    f"TRT engine export success, saved as {engine_file} ({file_size(engine_file):.1f} MB)"
+                )
+
+    def allocate_buffers(self, engine, is_explicit_batch=False, dynamic_shapes=[]):
+        inputs = []
+        outputs = []
+        bindings = []
+
+        class HostDeviceMem(object):
+            def __init__(self, host_mem, device_mem):
+                self.host = host_mem
+                self.device = device_mem
+
+            def __str__(self):
+                return "Host:\n" + str(self.host) + "\nDevice:\n" + str(self.device)
+
+            def __repr__(self):
+                return self.__str__()
+
+        for binding in engine:
+            dims = engine.get_binding_shape(binding)
+            print(dims)
+            if dims[0] == -1:
+                assert len(dynamic_shapes) > 0
+                dims[0] = dynamic_shapes[0]
+            size = trt.volume(dims) * engine.max_batch_size
+            dtype = trt.nptype(engine.get_binding_dtype(binding))
+            # Allocate host and device buffers
+            host_mem = cuda.pagelocked_empty(size, dtype)
+            device_mem = cuda.mem_alloc(host_mem.nbytes)
+            # Append the device buffer to device bindings.
+            bindings.append(int(device_mem))
+            # Append to the appropriate list.
+            if engine.binding_is_input(binding):
+                inputs.append(HostDeviceMem(host_mem, device_mem))
+            else:
+                outputs.append(HostDeviceMem(host_mem, device_mem))
+        return inputs, outputs, bindings
--- pancake/utils/general.py
+++ pancake/utils/general.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 import cv2
 import numpy as np
+import onnx
 import pandas as pd
 import torch
 import torchvision
@@ -26,7 +27,6 @@ from .torch_utils import init_torch_seeds
 from pancake.logger import setup_logger
 
 l = setup_logger(__name__)
-
 
 # Settings
 torch.set_printoptions(linewidth=320, precision=5, profile="long")
@@ -901,3 +901,61 @@ def resize_aspectratio(image, width=None, height=None, inter=cv2.INTER_AREA):
         dim = (width, int(h * r))
 
     return cv2.resize(image, dim, interpolation=inter)
+
+
+def export_onnx(
+    model, 
+    onnx_path, 
+    input_tensor, 
+    dynamic_axes: bool=True, 
+    simplify: bool=True
+    ):
+    """
+    :param model: PyTorch model
+    :param onnx_path: target location for new onnx model
+    :param input: tensor with input data size
+    """
+    if os.path.isfile(onnx_path):
+        l.info(f"Found a corresponding ONNX model at {onnx_path}")
+        return True
+
+    try:
+        torch.onnx.export(
+            model, input_tensor, onnx_path, 
+            input_names=["input"], output_names=["output"], 
+            opset_version=11,
+            operator_export_type=torch.onnx.OperatorExportTypes.ONNX,
+            training=torch.onnx.TrainingMode.EVAL,
+            do_constant_folding=False,
+            dynamic_axes={'input': {0: 'batch', 2: 'height', 3: 'width'},  # size(1,3,640,640)
+                          'output': {0: 'batch', 2: 'y', 3: 'x'}} if dynamic_axes else None,
+            verbose=False
+        )
+
+        # Checks
+        model_onnx = onnx.load(onnx_path)  # load onnx model
+        onnx.checker.check_model(model_onnx)  # check onnx model
+
+        # Simplify
+        if simplify:
+            try:
+                check_requirements(['onnx-simplifier'])
+                import onnxsim
+
+                l.info(f'Simplifying with onnx-simplifier {onnxsim.__version__}...')
+                model_onnx, check = onnxsim.simplify(
+                    model_onnx,
+                    dynamic_input_shape=dynamic_axes,
+                    input_shapes={'input': list(input_tensor.shape)} 
+                    if dynamic_axes else None)
+                assert check, 'assert check failed'
+                onnx.save(model_onnx, onnx_path)
+            except Exception as e:
+                l.info(f'Simplifier failure: {e}')
+                return
+
+        l.info(f'Export success, saved as {onnx_path} ({file_size(onnx_path):.1f} MB)')
+        return True
+    except Exception as e:
+        l.critical(f'Export failure: {e}')
+        return False
