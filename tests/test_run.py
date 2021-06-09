9c53c905c3401e28cbc4c435889d097c29395937
mauricesvp <maurice@walny.de>
2021-06-09 17:09:21 +0200

    Add optional profiling to test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,16 +1,31 @@
 import argparse
+import cProfile
+from pstats import SortKey
+
 from pancake import run
 
 
-def test_run(cfg_path: str = None):
-    run.main(cfg_path)
+def test_run(profile: bool = False):
+    if not profile:
+        run.main()
+        return
+
+    pr = cProfile.Profile()
+    pr.enable()
+
+    run.main()
+
+    pr.disable()
+    s = io.StringIO()
+    sortby = SortKey.CUMULATIVE
+    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps.print_stats()
+    print(s.getvalue())
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--cfg", nargs="?", type=str, default=None, help="pancake config path"
-    )
+    parser.add_argument("--profile", action="store_true", help="Use profiler")
     args = parser.parse_args()
 
-    test_run(args.cfg)
+    test_run(args.profile)
