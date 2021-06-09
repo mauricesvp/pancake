9234686a9837b2c2c1d8e87dd8b8a9677fa0a3ef
mauricesvp <maurice@walny.de>
2021-06-09 17:16:05 +0200

    Update run test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,24 +1,26 @@
 import argparse
 import cProfile
-from pstats import SortKey
+import io
+from pstats import SortKey, Stats
 
 from pancake import run
 
 
 def test_run(profile: bool = False):
+    n = 3
     if not profile:
-        run.main()
+        run.main(n=n)
         return
 
     pr = cProfile.Profile()
     pr.enable()
 
-    run.main()
+    run.main(n=n)
 
     pr.disable()
     s = io.StringIO()
     sortby = SortKey.CUMULATIVE
-    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    ps = Stats(pr, stream=s).sort_stats(sortby)
     ps.print_stats()
     print(s.getvalue())
 
