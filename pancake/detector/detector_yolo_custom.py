b96d3b6fca0dbf8a25996ce9d6d2c4ddc925f1bf
mauricesvp <maurice@walny.de>
2021-06-26 17:01:20 +0200

    add db to gitignore

--- pancake/detector/detector_yolo_custom.py
+++ pancake/detector/detector_yolo_custom.py
@@ -8,7 +8,6 @@ from .detector import Detector
 import pancake.models as m
 
 # from pancake.models.tensorrt.yolov5_tensorrt import Yolov5TRT
-from pancake.models.tensorrt.yolov5_trt_2 import Yolov5TRT
 from pancake.logger import setup_logger
 from pancake.utils.common import fix_path
 from pancake.utils.datasets import letterbox
@@ -50,11 +49,14 @@ class YOLOCustomDetector(Detector):
         )
 
         try:
-            self.model = (
-                Yolov5TRT(self.model, trt_engine_path, trt_plugin_library)
-                if trt
-                else self.model
-            )
+            if trt:
+                from pancake.models.tensorrt.yolov5_trt_2 import Yolov5TRT
+
+                self.model = (
+                    Yolov5TRT(self.model, trt_engine_path, trt_plugin_library)
+                    if trt
+                    else self.model
+                )
         except ModuleNotFoundError:
             l.info(f"Will fallback to weights file: {weights}")
 
@@ -93,9 +95,11 @@ class YOLOCustomDetector(Detector):
 
         # Padded resize
         pr_imgs = [
-            letterbox(x, self.model._required_img_size, stride=self.model._stride)[0]
+            # letterbox(x, self.model._required_img_size, stride=self.model._stride)[0]
+            letterbox(x, self.model._required_img_size, stride=32)[0]
             for x in imgs
         ]
+        print([x.shape for x in pr_imgs])
         # Stack
         pr_imgs = np.stack(pr_imgs, 0)
         # Convert
